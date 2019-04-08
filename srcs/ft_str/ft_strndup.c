2b8c4a2607efc2b8b54b80a3af1c5fc6f00d606a
juochen <pootitan@gmail.com>
2019-04-08 00:05:32 -0700

    v.2019.4.9.42sh: add dict_iter

--- srcs/ft_str/ft_strndup.c
+++ srcs/ft_str/ft_strndup.c
@@ -1,41 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juochen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/22 18:44:24 by juochen           #+#    #+#             */
-/*   Updated: 2018/02/28 21:13:26 by juochen          ###   ########.fr       */
+/*   Created: 2018/02/20 17:18:53 by juochen           #+#    #+#             */
+/*   Updated: 2018/02/25 14:57:46 by juochen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libft.h"
 
-char		**ft_strsplit(char const *s, char c)
+char		*ft_strndup(const char *s1, size_t n)
 {
-	char	**words;
-	int		*pos;
-	int		wcount;
-	int		i;
-	int		j;
+	size_t 	s1_len;
+	char	*s2;
+	size_t	s2_len;
+	size_t	i;
 
-	if (!s)
+	if (s1 == NULL)
 		return (NULL);
-	wcount = ft_words_count(s, c);
-	words = (char **)malloc(sizeof(char *) * (wcount + 1));
-	if (!words)
+	s1_len = ft_strlen(s1);
+	s2_len = (s1_len < n ? s1_len : n);
+	s2 = (char *)malloc(sizeof(char) * s2_len + 1);
+	if (s2 == NULL)
 		return (NULL);
-	pos = ft_words_pos(s, c, wcount);
-	i = -1;
-	while (++i < wcount)
+	i = 0;
+	while (i < s2_len)
 	{
-		words[i] = ft_strnew(pos[i * 2 + 1] - pos[i * 2]);
-		j = -1;
-		while (++j + pos[i * 2] < pos[i * 2 + 1])
-			words[i][j] = s[j + pos[i * 2]];
+		s2[i] = s1[i];
+		i++;
 	}
-	words[i] = NULL;
-	free(pos);
-	return (words);
+	s2[i] = '\0';
+	return (s2);
 }
